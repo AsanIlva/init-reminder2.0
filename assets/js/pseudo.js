c7c69686c60887c5442ad862b444af26c2c5df8f
Pasan De Silva <pasanderaw1212@gmail.com>
2021-10-11 23:58:52 +1030

    first commit

---
+++ assets/js/pseudo.js
@@ -0,0 +1,33 @@
+//Todo in HTML => div that will hold the time table, A div or header that shows the current day
+//JavaScript has a DateObject() built in
+//Input fields inside the div for the eight hours in the day
+
+//When the page loads, check local storage,
+//if appointments exists, load it into the corresponding time slot
+
+//I'll need a function to capture the user input and saving it in local storage
+//each input field will need it's own save button
+
+//I'll need way to track the current day and time.  
+
+//A formula that will color code an hour that has passed
+
+//A conditional statment.
+// if the div id is greater than or equal to the time style background color green else color grey
+// do this in a loop
+
+// get the element that the info goes into
+
+// decide on what it should look like
+// and then update it...
+// should this run in a loop and update the clock reguraly?
+// create timeblocks with inputs and buttons
+// loop and dynamically or I could hard code it.
+// add events to the submit button to save what is in the
+//     input to local storage
+//  get the value out of local storage and populate
+//     the input with the data
+// check to see what the color of the input should be based on the time
+//      moment?
+// look at the css for classes later for this
+// store data in localstrage as an object where the key is the hour of the day...
